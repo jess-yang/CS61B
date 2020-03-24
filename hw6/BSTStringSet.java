d619889f95f4d19fe285b971cc8109d753f0554d
Jessica Yang <jessica.yang@berkeley.edu>
2020-03-23 21:00:14 -0700

    hw6 pt a&b

--- hw6/BSTStringSet.java
+++ hw6/BSTStringSet.java
@@ -1,12 +1,8 @@
-import java.util.Iterator;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.Stack;
+import java.util.*;
 
 /**
  * Implementation of a BST based String Set.
- * @author
+ * @author Jessica Yang
  */
 public class BSTStringSet implements StringSet, Iterable<String> {
     /** Creates a new empty set. */
@@ -16,18 +12,59 @@ public class BSTStringSet implements StringSet, Iterable<String> {
 
     @Override
     public void put(String s) {
-        // FIXME: PART A
+        Node last = find(s);
+        if (last == null) {
+            _root = new Node(s);
+        } else {
+            int c = s.compareTo(last.s);
+            if (c < 0) {
+                last.left = new Node(s);
+            } else if (c > 0) {
+                last.right = new Node(s);
+            }
+        }
     }
 
     @Override
     public boolean contains(String s) {
-        return false; // FIXME: PART A
+        Node last = find(s);
+        return last != null && s.equals(last.s);
+    }
+
+    private Node find(String s) {
+        if (_root == null) {
+            return null;
+        }
+        Node p;
+        p = _root;
+        while (true) {
+            int c = s.compareTo(p.s);
+            Node next;
+            if (c < 0) {
+                next = p.left;
+            } else if (c > 0) {
+                next = p.right;
+            } else {
+                return p;
+            }
+            if (next == null) {
+                return p;
+            } else {
+                p = next;
+            }
+        }
     }
 
     @Override
     public List<String> asList() {
-        return null; // FIXME: PART A
+        BSTIterator bst = new BSTIterator(_root);
+        List<String> ar = new ArrayList<>();
+        while (bst.hasNext()) {
+            ar.add(bst.next());
+        }
+        return ar;
     }
+
 
 
     /** Represents a single Node of the tree. */
@@ -90,15 +127,59 @@ public class BSTStringSet implements StringSet, Iterable<String> {
         }
     }
 
+    /** An iterator over a BSTStringSet with bounds. */
+    private static class BSTIteratorRange implements Iterator<String> {
+
+        private Stack<Node> _toDo = new Stack<>();
+        private String _low;
+        private String _high;
+
+        BSTIteratorRange(Node node, String low, String high) {
+            _low = low;
+            _high = high;
+            addTree(node);
+        }
+
+        @Override
+        public boolean hasNext() {
+            return !_toDo.empty();
+        }
+
+        @Override
+        public String next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            Node node = _toDo.pop();
+            return node.s;
+        }
+
+        private void addTree(Node n) {
+            if (n == null) {
+                return;
+            }
+            else if (n.s.compareTo(_low) < 0) {
+                addTree(n.right);
+            } else if (n.s.compareTo(_high) > 0) {
+                addTree(n.left);
+            } else {
+                addTree(n.right);
+                _toDo.push(n);
+                addTree(n.left);
+            }
+        }
+
+    }
+
     @Override
     public Iterator<String> iterator() {
         return new BSTIterator(_root);
     }
 
-    // FIXME: UNCOMMENT THE NEXT LINE FOR PART B
-    // @Override
+
+    @Override
     public Iterator<String> iterator(String low, String high) {
-        return null;  // FIXME: PART B
+        return new BSTIteratorRange(_root, low, high);
     }
 
 
