4d4595c5380f22e188ebb2b34ff692cf229797f0
Jessica Yang <jessica.yang@berkeley.edu>
2020-04-06 23:17:15 -0700

    done

--- proj2/loa/BoardWidget.java
+++ proj2/loa/BoardWidget.java
@@ -99,16 +99,16 @@ class BoardWidget extends Pad {
     private void drawPiece(Graphics2D g, Square s) {
         Piece p = _board.get(s);
         switch (p) {
-            case EMP:
-                return;
-            case WP:
-                g.setColor(WHITE_COLOR);
-                break;
-            case BP:
-                g.setColor(BLACK_COLOR);
-                break;
-            default:
-                assert false;
+        case EMP:
+            return;
+        case WP:
+            g.setColor(WHITE_COLOR);
+            break;
+        case BP:
+            g.setColor(BLACK_COLOR);
+            break;
+        default:
+            assert false;
         }
         g.fillOval(cx(s) + PIECE_OFFSET, cy(s) + PIECE_OFFSET,
                 PIECE_SIZE, PIECE_SIZE);
@@ -129,7 +129,7 @@ class BoardWidget extends Pad {
     /** Handle a mouse-button release on S. */
     private void mouseReleased(Square s) {
         Move move = Move.mv(_from, s);
-        if (move != null && _board.isLegal(move)){
+        if (move != null && _board.isLegal(move)) {
             _commands.add(move.toString());
         }
         _from = null;
@@ -145,14 +145,14 @@ class BoardWidget extends Pad {
                 && x >= 0 && x < SIZE && y >= 0 && y < SIZE) {
             Square s = sq(x, y);
             switch (e.getID()) {
-                case MouseEvent.MOUSE_PRESSED:
-                    mousePressed(s);
-                    break;
-                case MouseEvent.MOUSE_RELEASED:
-                    mouseReleased(s);
-                    break;
-                default:
-                    break;
+            case MouseEvent.MOUSE_PRESSED:
+                mousePressed(s);
+                break;
+            case MouseEvent.MOUSE_RELEASED:
+                mouseReleased(s);
+                break;
+            default:
+                break;
             }
         }
     }
